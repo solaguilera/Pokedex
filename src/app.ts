b89f86216441034b850ee873b69a9cd07f54c1da
solaguilera <solaguilera@pm.me>
2021-08-23 12:28:59 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+const container: HTMLElement | any = document.getElementById('app');
+const pokemons: number = 100;
+
+interface IPokemon {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+}
+
+
